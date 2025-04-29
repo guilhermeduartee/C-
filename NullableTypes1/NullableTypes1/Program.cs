adbdd170bebb559288177cd03e8e26e3707e7e3c
Guilherme Duarte <guilherme.azev@outlook.com>
2025-04-29 08:33:15 -0300

    Add files via upload

---
+++ NullableTypes1/NullableTypes1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("### Nullable Types ##\n");
+//Nullable<T><nome>=null;   Nullable<bool>b=null;
+
+Nullable<int> i = null;
+Nullable<double> d = null;
+Nullable<bool> b = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(b);
+
+//Para simplificar pode se utilizar o operador "?" EX: Int? i = null
+
+int? t = null;
+double? g = null;
+bool? v = null;
+
+Console.WriteLine(t);
+Console.WriteLine(g);
+Console.WriteLine(v);
+
+// Utilizar o operador "??" para atribuir um tipo anulável a um tipo não anulável
+
+int? a = null;
+int e = a  ??  0;
+Console.WriteLine(e);
+
+Console.ReadKey();
---
+++ TipoDateTime1/TipoDateTime1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Struct DateTime ##\n"); 
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+//Extrair informações da data atual
+
+DateTime hoje = DateTime.Now;
+
+Console.WriteLine(hoje.Year);
+Console.WriteLine(hoje.Month);
+Console.WriteLine(hoje.Day);
+Console.WriteLine(hoje.Hour);
+Console.WriteLine(hoje.Minute);
+Console.WriteLine(hoje.Second);
+Console.WriteLine(hoje.Millisecond);
+
+//Adicionando valores
+Console.WriteLine($"hoje : {hoje}\n");
+Console.WriteLine(hoje.AddDays(30));
+Console.WriteLine(hoje.AddMonths(1));
+Console.WriteLine(hoje.AddHours(2));
+Console.WriteLine(hoje.AddYears(5));
+
+//obter o dia da semana e do ano
+Console.WriteLine(hoje.DayOfWeek);
+Console.WriteLine(hoje.DayOfYear);
+
+//data no formato longo e curo
+Console.WriteLine(hoje.ToLongDateString());
+Console.WriteLine(hoje.ToShortDateString());
+
+Console.WriteLine(hoje.ToLongTimeString());
+Console.WriteLine(hoje.ToShortTimeString());
+
+Console.ReadKey();
